2302f88f93a514c6bfbc777bbb343ab10c4f09e7
mil3zh <milena.lazzaretti5@gmail.com>
2020-12-09 22:03:53 +0100

    css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,25 +2,28 @@ import React, { Component } from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Form, FormControl, Button } from 'react-bootstrap';
 
 const NavBar = () => {
-    return (<Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    return (<Navbar bg="danger" expand="lg">
+    <Navbar.Brand href="#home">The Book</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
+      {/* <NavDropdown title="Contribute" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Add a word</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Edit a word</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Write to us</NavDropdown.Item>
+      </NavDropdown> */}
+        <Nav.Link href="#home">Write</Nav.Link>
+        <Nav.Link href="#home">Edit</Nav.Link>
+        <Nav.Link href="#link">Team</Nav.Link>
       </Nav>
-      <Form inline>
+      {/* <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
         <Button variant="outline-success">Search</Button>
-      </Form>
+      </Form> */}
+      <Nav className="ml-auto">
+        <Nav.Link href="#link">笔</Nav.Link>
+      </Nav>
     </Navbar.Collapse>
   </Navbar>)
 };
