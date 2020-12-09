39f880271c49ba26d75590cda70b7c6816b08759
mil3zh <milena.lazzaretti5@gmail.com>
2020-12-09 22:04:38 +0100

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../index.css'
+
+const Footer = () => (
+  <div className="footer">
+    <p>&copy; Zhang Zihan - All rights reserved </p>
+  </div>
+);
+
+export default Footer;
